2a25dd2cee70770e76102933ad01c1dd40362703
Leonardo <alu3270@etsii.ull.es>
2012-09-26 10:32:55 +0100

    Imagenes y Template

--- rpsCAS.rb
+++ rpsCAS.rb
@@ -1,9 +1,8 @@
 require 'sinatra'
 require 'erb'
 
-# before we process a route we'll set the response as plain text
-# and set up an array of viable moves that a player (and the
-# computer) can perform
+set :public_forder, File.dirname(__FILE__)
+
 before do
   @defeat = { rock: :scissors, paper: :rock, scissors: :paper}
   @throws = @defeat.keys
@@ -16,36 +15,41 @@ end
 get '/throw' do
    erb :index
 end
+
+def image_throw ima_t
+  if ima_t == :rock
+	images= "/images/rock.jpg"
+  elsif ima_t == :scissors
+	images= "/images/scissors.jpg"
+  elsif ima_t == :paper
+	images= "/images/paper.jpg"
+  end
+end
+
 post '/throw' do
   # the params hash stores querystring and form data
    
   @player_throw = params[:player_throw].to_sym
+  @images_player = image_throw @player_throw
 
   halt(403, "You must throw one of the following: '#{@throws.join(', ')}'") unless @throws.include? @player_throw
 
   @computer_throw = @throws.sample
+  @images_computer = image_throw @computer_throw
 
+  
   if @player_throw == @computer_throw 
     @answer = "Empatas"
-    erb :index
+    @images = image_throw @player_throw
   elsif @player_throw == @defeat[@computer_throw]
     @answer = "Computer Gana; #{@computer_throw} defeats #{@player_throw}"
-    erb :index
+    @images = image_throw @computer_throw
   else
     @answer = "Bien Hecho. #{@player_throw} ha ganado #{@computer_throw}"
-    erb :index
+    @images = image_throw @player_throw
   end
-end
-__END__
+erb :myTemplate, :layout => :mylayout
 
-@@index
-<html>
-  <head>
-    <title>Rock Paper Scissors</title>
-  </head>
-  <body>
-    <h2> Computer chooses:  <%= @computer_throw %> </h2>
-    <h2> You choose: <%= @player_throw %> </h2>
-    <h1> <%= @answer %> </h1>
-  </body>
-</html>
+
+
+end
